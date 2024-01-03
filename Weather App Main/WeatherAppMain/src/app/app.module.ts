e6dea083a1be97e2fb1ab4386e4fc899f37d3c79
MikeMawira <43206098+MikeMawira@users.noreply.github.com>
2024-01-03 12:14:57 +0300

    first-commit

---
+++ Weather App Main/WeatherAppMain/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LeftContainerComponent } from './left-container/left-container.component';
+import { RightContainerComponent } from './right-container/right-container.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeftContainerComponent,
+    RightContainerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
